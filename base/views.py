92a1ffa0e73e94abd3e267b1f0cb3404379bccea
shrishail-0127 <shriiipj420@gmail.com>
2024-08-17 17:26:38 +0530

    initial commit

---
+++ base/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Room
+
+# Create your views here.
+
+
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html',context)
+
+def room(request,pk):
+    room = Room.objects.get(id=pk)
+    print(room)
+    context = {'room':room}
+
+    return render(request, 'base/room.html',context)
+
+

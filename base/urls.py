47704c54665c90645da3e891ad7cb2ab42d1f113
shrishail-0127 <shriiipj420@gmail.com>
2024-08-18 18:38:37 +0530

    login page

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     
+    path('login/',views.loginPage, name="login"),
+
     path('',views.home,name='home'),
     path('room/<int:pk>/', views.room, name='room'),
     path('create-room/',views.createRoom, name="create-room"),
--- base/views.py
+++ base/views.py
@@ -1,11 +1,35 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.db.models  import Q
 from .models import Room, Topic
 from . forms import RoomForm
 
 # Create your views here.
 
+def loginPage(request):
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "user does not exist.")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request, "username and passwords not exist")
+
+    
+    return render(request, 'base/login_register.html')
 
 
 def home(request):

e61cee6c5d7cb0ab157dafcd7841794268573f9e
shrishail pujeri <shrishail@nanokernel.net>
2024-09-27 16:57:58 +0530

    recent commit

--- studybud/settings.py
+++ studybud/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-y&m93&-9wh*yu1mgx-!n6rf@a@lm=q3i$=s(jqfxk_^zf5ya$d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0.1", "localhost", "127.0.0.1", "[::1]"]
 
 
 # Application definition
